e6847412db6440af31395cf315f812effa45fac2
WxAaRoNxW <WxAaRoNxW@yahoo.com>
2026-08-31 00:19:13 +0800

    feat: add GVU config automatically in build

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 import subprocess
 
-from config.config import custom_script_name, internals_name, LOCALE_PATH
+from config.config import custom_script_name, internals_name, LOCALE_PATH, ORIGINAL_FILES_PATH, CRACKED_FILES_PATH, gvu_config_dir, CONFIG_COPY_PATH
 
 subprocess.run([
     "pyinstaller",
@@ -9,5 +10,16 @@ subprocess.run([
     "--contents-directory", internals_name,
     "--uac-admin",
     "--add-data", f"{LOCALE_PATH}:locale",
+    "--add-data", f"{CONFIG_COPY_PATH}:{custom_script_name} config",
     "main.py",
 ], check=True)
+
+
+# Place empty dirs after PyInstaller finishes
+output_dir = Path("dist") / custom_script_name / internals_name / gvu_config_dir
+
+for directory in [
+    output_dir / ORIGINAL_FILES_PATH.name,
+    output_dir / CRACKED_FILES_PATH.name
+]:
+    directory.mkdir(parents=True, exist_ok=True)
